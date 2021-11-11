75774cca67896ffcf45918624ac35b71cec2ab3d
mariasalud <86627563+mariasalud@users.noreply.github.com>
2021-11-11 09:32:46 -0600

    Uploaded the footer.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from "react";
 import Weather from "./Weather";
-
-
+import Footer from "./Footer";
 
 import './App.css';
-
-
 
 function App() {
   return (
     <div className="App">
       <Weather />
+      <Footer />
     </div>
-   
-    );
+  );
 }
 
 export default App;
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+return (
+    <div className="footer">
+        <a href="https://github.com/mariasalud/weather-project-react" target="_blank"> 
+        Open-soure code
+         </a> By: Maria Garcia
+     </div>
+);
+}
+
