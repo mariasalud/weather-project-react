fab130f074dab805627db72076098adea9ace2b8
mariasalud <86627563+mariasalud@users.noreply.github.com>
2021-11-11 08:16:12 -0600

    Made no changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import React from "react";
 import ReactDom from "react-dom";
 import Weather from "./Weather";
 
+
+
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <Weather />
     </div>
-  );
+   
+    );
 }
 
 export default App;
