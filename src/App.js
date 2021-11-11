6c87f248b6c11d86d24331238b2c9909ce65491b
mariasalud <86627563+mariasalud@users.noreply.github.com>
2021-11-11 09:59:08 -0600

    Included a br.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Weather />
+      <br />
       <Footer />
     </div>
   );
--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,10 @@ import React from "react";
 export default function Footer() {
 return (
     <div className="footer">
-        <a href="https://github.com/mariasalud/weather-project-react" target="_blank"> 
-        Open-soure code
+        <a href="https://github.com/mariasalud/weather-project-react" target="_blank" rel="noreferrer"> 
+        GitHub repository
+         </a> and <a href="https://cranky-stonebraker-d300e4.netlify.app/" target="_blank" rel="noreferrer"> 
+         Netlify
          </a> By: Maria Garcia
      </div>
 );
