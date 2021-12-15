cad524292e8a974f0acf107866ed4db9af6f5992
mariasalud <86627563+mariasalud@users.noreply.github.com>
2021-12-15 14:30:58 -0600

    Up dated the footer.

--- src/Footer.js
+++ src/Footer.js
@@ -3,11 +3,14 @@ import React from "react";
 export default function Footer() {
 return (
     <div className="footer">
+        <div className="container">
+        This project was coded by Maria Garcia {""}
         <a href="https://github.com/mariasalud/weather-project-react" target="_blank" rel="noreferrer"> 
         GitHub repository
          </a> and <a href="https://cranky-stonebraker-d300e4.netlify.app/" target="_blank" rel="noreferrer"> 
          Netlify
-         </a> By: Maria Garcia
+         </a> 
+         </div>
      </div>
 );
 }
